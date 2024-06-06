c4017a71de6de313b1a9bcd78e7a58df6d748eab
Izaaq <odongoizaaq@gmail.com>
2024-06-06 08:39:11 -0400

    Adds the commit

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,37 @@
 class Stack:
 
-    def __init__(self, items = [], limit = 100):
-        pass
+    def __init__(self, items=None, limit=100):
+        self.items = items if items is not None else []
+        self.limit = limit
 
     def isEmpty(self):
-        pass
+        return len(self.items) == 0
 
     def push(self, item):
-        pass
+        if not self.full():
+            self.items.append(item)
 
     def pop(self):
-        pass
-
+        if not self.isEmpty():
+            return self.items.pop()
+        else:
+            return None
+  
     def peek(self):
-        pass
+        if not self.isEmpty():
+            return self.items[-1]
+        else:
+            return None
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        return len(self.items) == self.limit
 
-    def search(self, target):
-        pass
+    def search(self, value):
+        try:
+            index = self.items.index(value)
+            return len(self.items) - index - 1
+        except ValueError:
+            return -1
